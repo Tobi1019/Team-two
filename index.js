1244838e8a8c9d1ea27f6dcd9e3916fe05642485
Tobi Williams <88983600+Tobi1019@users.noreply.github.com>
2022-06-26 15:54:31 +0100

    Update index.js
    .

--- index.js
+++ index.js
@@ -12,9 +12,4 @@ menu.onclick = () => {
     navbar.classList.toggle('active');
 };
 
-window.onscroll = () => {
-    menu.classList.remove('bx-x');
-    navbar.classList.remove('active');
-}
-$('#dropdown-btn').click (function(
-     $('dropdown-menu-display').toggleClass('menu-display');
+     
